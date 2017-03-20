6c6e2669968f3d9bcbadac225c6a5dcfd3b0a54c
TarikHuber <huber.tarik@gmail.com>
2017-03-20 10:42:14 +0100

    Updates

--- src/utils/drawerHelper.js
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_BREACK_POINT = 'medium';
-
-export default function isResponsiveAndOverBreackPoint(browser, responsiveDrawer, breackPoint){
-  return browser.greaterThan[breackPoint!==undefined?breackPoint:DEFAULT_BREACK_POINT] && responsiveDrawer.responsive;
-}
